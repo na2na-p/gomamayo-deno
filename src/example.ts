51eafe8ed4dc230709a95dd61ad68c0e76b23942
na2na-p <yamamoto.akyshi@gmail.com>
2022-03-21 22:51:43 +0900

    クラス化した

--- src/example.ts
+++ src/example.ts
@@ -1,4 +1,6 @@
-import * as gomamayo from "https://deno.land/x/gomamayo_deno/mod.ts";
+import {Gomamayo} from "../mod.ts";
+
+const gomamayo = new Gomamayo();
 
 const inputString:string = Deno.args[0];
 
